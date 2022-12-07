b271fcd4be7a98cad755c239a44a9369c7192a16
atrus <atrusdido@gmail.com>
2022-12-07 18:07:27 +0300

    train

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p> kkdkdk eslkfkanflkslfgn</p>
+        <p> kkdkdk </p>
         <a
           className="App-link"
           href="https://reactjs.org"
